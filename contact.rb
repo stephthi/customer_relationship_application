1ce26341663bfa130660de3644ae467e51c498db
stephthi <nstephanie2@gmail.com>
2015-10-30 13:59:56 -0400

    Added Contact and CRM file with function add Contact

---
+++ contact.rb
@@ -0,0 +1,61 @@
+# class CRM
+# 	attr_accessor :title
+
+# 	def self.run(name)
+# 		crm = new(name)
+
+# 	end
+
+class	Contact
+	attr_accessor	:first_name, :last_name, :email, :notes
+	attr_reader :id
+
+	@@contacts = []
+	@@id = 1
+
+# Options need to be a hash as a minimum or it will crash.
+
+	# def initialize(options = {})
+	# 	@first_name = options[:first_name]
+	# 	@last_name = options[:last_name]
+	# 	@email = options[:email]
+	# 	@notes = options[:note]
+	# end
+
+	# Could also do to make FN/LN mandatory:
+	def initialize(first_name, last_name, options = {})
+		@first_name = first_name
+		@last_name =last_name
+		@email = options[:email]
+		@notes = options[:note]
+
+		@id = @@id
+		@@id += 1
+	end
+
+	#You can also put Contact.new below. Don't need variables in brackets.
+	#Took it out to put it in class and initializer.
+	def self.create(first_name, last_name, options = {})
+		new_contact = Contact.new(first_name, last_name, options)
+		# new_contact.id = @@id
+		# @@id += 1
+		@@contacts << new_contact
+		puts "Contact added at #{@@id-1}."
+	end
+# attr_accessor creates these methods
+	# def first_name
+	# 	@first_name
+	# end
+
+	# def first_name=(new_first_name)
+	# 	@first_name = new_first_name
+	# end
+
+	def self.all
+		@@contacts
+	end
+
+	def full_name
+		"#{first_name} #{last_name}"
+	end
+end
---
+++ crm.rb
@@ -0,0 +1,73 @@
+require_relative 'contact.rb'
+
+class CRM
+	def initialize(name)
+		@name = name
+	end
+
+	def print_main_menu
+		puts "1. Add a contact"
+		puts "2. Modify a contact"
+		puts "3. Display all contacts"
+		puts "4. Display contact"
+		puts "5. Display contact attribute"
+		puts "6. Delete a contact"
+		puts "7. Exit"
+	end
+
+	#infinite loop
+	def main_menu
+		while true
+			print_main_menu
+			print "Choose an option: "
+			user_input = gets.chomp.to_i
+			break if user_input == 7
+			choose_option(user_input)
+	end
+end
+
+		#input = user_input
+	def choose_option(input)
+		case input
+		when 1 then add_contact
+		when 2 then modify_contact
+		when 3 then display_all_contacts
+		when 4 then display_contact
+		when 5 then display_contact attribute
+		when 6 then delete_contact
+		when 7 then exit
+		else
+			puts "Sorry, I'm afraid you can't do that"
+	end
+end
+
+	def add_contact
+		print "First name: "
+		first_name = gets.chomp
+
+		print "Last name: "
+		last_name = gets.chomp
+
+		print "Email: "
+		email = gets.chomp
+
+		print "Note: "
+		notes = gets.chomp
+
+		contact = Contact.create(first_name, last_name, email: email, notes: notes)
+	end
+
+	def print_all
+		self.each do |contact|
+			puts "#{contact.first_name} #{contact.last_name} has been added."
+	end
+end
+end
+
+
+my_crm = CRM.new('Bitmaker CRM')
+my_crm.main_menu
+
+
+
+
